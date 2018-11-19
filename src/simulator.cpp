f2c27c338a6d66016fd8bfc702d7a1ae114596bf
MikeIsMike <38946057+MikeIsMike@users.noreply.github.com>
2018-11-19 15:51:30 +0000

    new structure, still seg fault

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]){
     uint32_t instruction;
     index = MEMORY.set_instructions(argv[1]);
 
-    while(PROG_COUNTER != 0){
+    while((PROG_COUNTER != 0) || (PROG_COUNTER<(index*4+0x10000000))){
         instruction = MEMORY.get_instruction(PROG_COUNTER);
 
         return_code = execute_instruction(instruction);
@@ -32,6 +32,6 @@ int main(int argc, char* argv[]){
     for(int j = 0; j < 32; j++){
         cout << "Register " << j << " : " << REG[j] << endl;
     }
-    uint8_t exit_code = REG[2] & BYTE_MASK; 
+    uint8_t exit_code = REG[2] & BYTE_MASK;
     exit(exit_code);
 }
