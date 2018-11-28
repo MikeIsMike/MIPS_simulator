e09aec6c1217d5686800478ad9fbccb0d32a76a0
MikeIsMike <38946057+MikeIsMike@users.noreply.github.com>
2018-11-28 15:58:15 +0000

    try catch exceptions

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -6,36 +6,42 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    int return_code;
-    int index;
-    uint32_t instruction;
-    if(argc <= 1){
-        exit(-21);
-    }
-    index = MEMORY.set_instructions(argv[1]);
-
-    while(PROG_COUNTER != 0){
-        instruction = MEMORY.get_instruction(PROG_COUNTER);
-        return_code = execute_instruction(instruction);
-
-        if(return_code){
-        //IF THERE IS AN ERROR AND NO BRANCH DELAY, IMMEDIATELY THROW EXCEPTION
-            // cerr << "ERROR: " << return_code << endl;
-            // cerr << "Program Counter: " << PROG_COUNTER << endl;
-            // for(int j = 0; j < 32; j++){
-            // cerr << "Register " << j << " : " << REG[j] << endl;
-            // }
-            exit(return_code);
+    try{
+        int return_code;
+        int index;
+        uint32_t instruction;
+        if(argc <= 1){
+            exit(-21);
         }
+        index = MEMORY.set_instructions(argv[1]);
 
-        if(REG[0] != 0){
-            REG[0] = 0;
+        while(PROG_COUNTER != 0){
+            instruction = MEMORY.get_instruction(PROG_COUNTER);
+            return_code = execute_instruction(instruction);
+
+            if(return_code){
+            //IF THERE IS AN ERROR AND NO BRANCH DELAY, IMMEDIATELY THROW EXCEPTION
+                // cerr << "ERROR: " << return_code << endl;
+                // cerr << "Program Counter: " << PROG_COUNTER << endl;
+                // for(int j = 0; j < 32; j++){
+                // cerr << "Register " << j << " : " << REG[j] << endl;
+                // }
+                exit(return_code);
+            }
+
+            if(REG[0] != 0){
+                REG[0] = 0;
+            }
         }
+        // cerr << "Program Counter: " << PROG_COUNTER << endl;
+        // for(int j = 0; j < 32; j++){
+        //     cerr << "Register " << j << " : " << REG[j] << endl;
+        // }
+        uint8_t exit_code = REG[2] & BYTE_MASK;
+        exit(exit_code);
     }
-    // cerr << "Program Counter: " << PROG_COUNTER << endl;
-    // for(int j = 0; j < 32; j++){
-    //     cerr << "Register " << j << " : " << REG[j] << endl;
-    // }
-    uint8_t exit_code = REG[2] & BYTE_MASK;
-    exit(exit_code);
+    catch(std::exception& e){
+        std::exit(-20);
+    }
+
 }
